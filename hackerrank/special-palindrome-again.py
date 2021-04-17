fb443f6da811b3c738bab82d304fe7c93daf9720
Rachit Sharma <rachit995@rocketmail.com>
2021-04-17 13:31:34 +0530

    completed in 3ms

---
+++ hackerrank/special-palindrome-again.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from itertools import combinations
+from collections import defaultdict
+
+# Complete the substrCount function below.
+
+
+def substrCount(n, s):
+
+
+if __name__ == '__main__':
+    s = "mnonopoo"
+    result = substrCount(len(s), s)
+    print(result)
